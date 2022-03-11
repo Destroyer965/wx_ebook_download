b81f191ec46a487ebf5f3540e9c4b6a741ee208e
Destroyer965 <52589269+Destroyer965@users.noreply.github.com>
2022-03-11 21:24:43 +0800

    Update config.js

--- js_sdk/yushijie-ossutil/ossutil/config.js
+++ js_sdk/yushijie-ossutil/ossutil/config.js
@@ -2,8 +2,8 @@ var fileHost = 'https://wx-ebook-download.oss-cn-chengdu.aliyuncs.com/';//你的
 var config = {
    //aliyun OSS config
   uploadImageUrl: `${fileHost}`, // 默认存在根目录，可根据需求改
-  AccessKeySecret: 'M8Yj8kawYXHdBhjJMXTaAsGW2sWxqm',        // AccessKeySecret 去你的阿里云上控制台上找
-  OSSAccessKeyId: 'LTAI5tDsRrxvuHkQEfdSvmZ6',         // AccessKeyId 去你的阿里云上控制台上找
+  AccessKeySecret: 'xxxxxx',        // AccessKeySecret 去你的阿里云上控制台上找
+  OSSAccessKeyId: 'xxxxxx',         // AccessKeyId 去你的阿里云上控制台上找
   timeout: 87600 //这个是上传文件时Policy的失效时间
 };
 module.exports = config
